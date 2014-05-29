06d7fd9728fa81151cc2aa3d0ec95ef671668c33
Rui Wu <ruiwu2014@users.noreply.github.com>
2014-05-29 10:12:02 -0400

    Update Single Number II.py

--- Single Number II.py
+++ Single Number II.py
@@ -11,7 +11,7 @@ class Solution:
             for i in xrange(31):
                 if bit[i]%3==1:res += 1<<i
         else:
-            for i in xrange(31):
+            for i in xrange(31): #negative
                 if bit[i]%3==0:res += 1<<i
             res =-(res +1)
         return res
