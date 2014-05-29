abad7210dafa689130c8903026cfaafbc3a93a63
rui <wurui109@gmail.com>
2014-05-29 10:11:04 -0400

    done

---
+++ Single Number II.py
@@ -0,0 +1,19 @@
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        bit = [0 for i in xrange(32)]
+        for x in A:
+            for i in xrange(32):
+                if x & (1<<i) == 1<<i: bit[i]+=1
+        res = 0
+        if bit[31]%3 == 0: #positive
+            for i in xrange(31):
+                if bit[i]%3==1:res += 1<<i
+        else:
+            for i in xrange(31):
+                if bit[i]%3==0:res += 1<<i
+            res =-(res +1)
+        return res
+
+
